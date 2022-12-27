a726e63fec674e4454ce091236271062a39de83a
ritul-mtalkz <ritul.mtalkz@gmail.com>
2022-12-27 15:49:17 +0530

    bug fix for image showing in url

--- index.js
+++ index.js
@@ -8,27 +8,44 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-let gfs;
+let gfs, gridfsBucket;
 connection();
 
 const conn = mongoose.connection;
 conn.once("open", function () {
+    gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+        bucketName: 'photos'
+    });
     gfs = Grid(conn.db, mongoose.mongo);
     gfs.collection("photos");
 });
 
+
 app.use("/file", upload);
 
 // media routes
-app.get("/file/:filename", async (req, res) => {
+app.get('/file/:filename', async (req, res) => {
     try {
         const file = await gfs.files.findOne({ filename: req.params.filename });
-        console.log(file.filename)
-        console.log(gfs.createReadStream)
-        const readStream = gfs.createReadStream(file.filename);
+        if (!file || file.length === 0) {
+            return res.status(404).json({
+                err: 'No file exists'
+            });
+        }
+        // Till version 5+ we were using this 
+        // const readStream = gfs.createReadStream(file.filename);
+        // readStream.pipe(response);
+        // For version 6+, Replace these two lines with the lines below
+
+        console.log(file.filename);
+        const readStream = gridfsBucket.openDownloadStream(file._id);
         readStream.pipe(res);
-    } catch (error) {
-        res.send("not found");
+    }
+    catch (error) {
+        res.status(404).json({
+            err: 'Not an image'
+        });
+        console.log("We got an error " + error);
     }
 });
 
